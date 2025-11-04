12bd7ea4fdea3b817ae9657abf80774d57c59cfa
IkerSada <ikersada2@gmail.com>
2025-11-04 20:36:30 +0100

    removed unused import

--- src/main/java/gui/BalorazioaIkusiGUI.java
+++ src/main/java/gui/BalorazioaIkusiGUI.java
@@ -3,7 +3,6 @@ package gui;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.List;
 import java.util.ResourceBundle;
 
 import javax.swing.JButton;
